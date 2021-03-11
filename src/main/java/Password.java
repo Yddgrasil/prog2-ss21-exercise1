a64a3c966d28fae4db4f569a1049740ce5a8fe07
Yddgrasil <71716001+Yddgrasil@users.noreply.github.com>
2021-03-11 21:27:41 +0100

    Probleme mit push gehabt

---
+++ src/main/java/Password.java
@@ -0,0 +1,58 @@
+
+
+    public class Password {
+
+        public static boolean checkPassword(String password) {
+
+            if (password.length() <8 || password.length() >25){
+                return false;
+            }
+
+            if( password.equals(password.toLowerCase()) ){
+                return false;
+
+            }
+
+            if( password.equals(password.toUpperCase()) ){
+                return false;
+            }
+            if (!password.matches(".*\\d.*")){
+                return false;
+            }
+
+            boolean containsSpecialCharacter = false;
+
+            for (char c : password.toCharArray()) {
+                if (")#$?!%/@".indexOf(c) >= 0) {
+                    containsSpecialCharacter = true;
+                }
+
+
+            }
+            return containsSpecialCharacter;
+
+
+     /*       boolean isConsecutiveNumber = false; {
+                char[] ch = new char[password.length()];
+                String[];
+                for(int i = 0; i < password.length() - 2; i++) {
+                    if (password[i + 1] == password[i] + 1 && password[i + 2] == password[i] + 2) return true;
+                    isConsecutiveNumber = true;
+                }
+
+
+                return false;
+            }
+
+      */
+
+
+
+        }
+
+
+
+
+    }
+
+
---
+++ src/test/java/checkPassword.java
@@ -0,0 +1,128 @@
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class checkPassword {
+    @BeforeAll
+    public static void init(){
+        System.out.println("Testing Your Password");
+    }
+
+    @AfterAll
+    public static void finish(){
+        System.out.println("Finished Testing Your Password");
+    }
+
+
+
+
+    @Test
+    public void TestPasswordnlyUpper()
+    {
+        String PasswordUppercase = "GDHJ$GHA3SFDG";
+
+        assertFalse(Password.checkPassword(PasswordUppercase));
+
+    }
+
+    @Test
+    public void TestPasswordOnlyLower()
+    {
+        String PasswordLowercase = "ahsj$dh4asdf";
+
+        assertFalse(Password.checkPassword(PasswordLowercase));
+
+    }
+
+    @Test
+    public void TestPasswordUpperAndLower()
+    {
+        String PasswordHasUpperAndLowercase= "DHJAQjh5a$sjdhk";
+
+        assertTrue(Password.checkPassword(PasswordHasUpperAndLowercase));
+
+    }
+
+
+
+    @Test
+    public void TestPasswordTooLong() {
+
+        String PasswordLong ="adfg5Sdkf$jieurioaisjfkdskh83498238574";
+
+        assertFalse(Password.checkPassword(PasswordLong));
+    }
+
+    @Test
+    public void TestPasswordTooShort() {
+        String PasswordShort = "sd$F4";
+
+        assertFalse(Password.checkPassword(PasswordShort));
+    }
+
+    @Test
+    public void TestPasswordGoodLength() {
+        String PasswordRightLength = "abcSd$fas5dfe";
+
+        assertTrue(Password.checkPassword(PasswordRightLength));
+    }
+
+
+    @Test
+    public void PasswordContainsNumber(){
+        String PasswordWithNumber ="hsadjfhas$jfh837HJ";
+
+        assertTrue(Password.checkPassword(PasswordWithNumber));
+    }
+
+    @Test
+    public void PasswordContainsNONumber(){
+        String PasswordNoNumber= "sadjfhdk$jasfhHashdfj";
+
+        assertFalse(Password.checkPassword(PasswordNoNumber));
+    }
+
+    @Test
+    public void PasswordContainsSpecialCharacter(){
+        String PasswordWithSpecialCharacter ="sadjfkhR%hj5Dhkjdsjf@";
+
+        assertTrue(Password.checkPassword(PasswordWithSpecialCharacter));
+    }
+
+    @Test
+    public void PasswordNoSpecialCharacter(){
+        String PasswordNotSpecial="asdjfhdskjahkd4WjshafkjZ&";
+
+        assertFalse(Password.checkPassword(PasswordNotSpecial));
+    }
+
+
+    @Test
+    public void PasswordIllegalSpecialCharacter€(){
+        String PasswordWrongSpecialCharacter ="sadjfkhR%hj5Dhkjds€jf@";
+        assertTrue(Password.checkPassword(PasswordWrongSpecialCharacter));
+    }
+
+    @Test
+    public void PasswordIllegalgSpecialCharacterTilde(){
+        String PasswordWrongSpecialCharacter ="sadjfkhR%hj5D~hkjds€jf@";
+
+        assertTrue(Password.checkPassword(PasswordWrongSpecialCharacter));
+    }
+
+
+
+
+   /* public void PasswordHasConsecutiveNumbers(){
+        String PasswordWithConsecutiveNumbers="hasdkjfhs@A5jksadfk123";
+        String PasswordWithNoConsecutiveNumbers="ufasidfuJUKJ@8901jdk";
+
+        assertTrue(Password.checkPassword(PasswordWithNoConsecutiveNumbers));
+        assertFalse(Password.checkPassword(PasswordWithConsecutiveNumbers));
+
+    }
+
+
+    */
+
+}
